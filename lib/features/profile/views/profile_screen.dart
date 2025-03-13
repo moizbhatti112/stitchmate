03ed84af1d9d17ed5345e63b6cacc9c720f327b4
moizbhatti112 <moizbhatti112@gmail.com>
2025-03-13 14:09:10 +0500

    profile update

--- lib/features/profile/views/profile_screen.dart
+++ lib/features/profile/views/profile_screen.dart
@@ -2,217 +2,172 @@ import 'package:flutter/material.dart';
 import 'package:gyde/features/gyde_ai/views/ai_welcome.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
-
 import 'package:gyde/core/constants/colors.dart';
+import 'package:provider/provider.dart';
 
-class ProfileScreen extends StatefulWidget {
-  const ProfileScreen({super.key});
-
-  @override
-  State<ProfileScreen> createState() => _ProfileScreenState();
-}
-
-class _ProfileScreenState extends State<ProfileScreen> {
-  final TextEditingController _fnameController = TextEditingController();
-  final TextEditingController _lnameController = TextEditingController();
-  final TextEditingController _cnameController = TextEditingController();
+class ProfileProvider extends ChangeNotifier {
+  final TextEditingController fnameController = TextEditingController();
+  final TextEditingController lnameController = TextEditingController();
+  final TextEditingController cnameController = TextEditingController();
   
-  bool isButtonEnabled = false;
   File? _image;
+  bool _isButtonEnabled = false;
 
-  @override
-  void initState() {
-    super.initState();
-    _fnameController.addListener(checkFields);
-    _lnameController.addListener(checkFields);
-    _cnameController.addListener(checkFields);
+  File? get image => _image;
+  bool get isButtonEnabled => _isButtonEnabled;
+
+  ProfileProvider() {
+    fnameController.addListener(checkFields);
+    lnameController.addListener(checkFields);
+    cnameController.addListener(checkFields);
   }
 
   @override
   void dispose() {
-    _fnameController.dispose();
-    _lnameController.dispose();
-    _cnameController.dispose();
+    fnameController.dispose();
+    lnameController.dispose();
+    cnameController.dispose();
     super.dispose();
   }
 
   void checkFields() {
-    setState(() {
-      isButtonEnabled = 
-        _fnameController.text.isNotEmpty &&
-        _lnameController.text.isNotEmpty &&
-        _cnameController.text.isNotEmpty &&
-         _image != null; 
-    });
+    _isButtonEnabled =
+        fnameController.text.isNotEmpty &&
+        lnameController.text.isNotEmpty &&
+        cnameController.text.isNotEmpty &&
+        _image != null;
+    notifyListeners();
   }
 
-  Future<void> _pickImage() async {
-    final pickedFile = await ImagePicker().pickImage(
-      source: ImageSource.gallery,
-    );
-
+  Future<void> pickImage() async {
+    final pickedFile = await ImagePicker().pickImage(source: ImageSource.gallery);
     if (pickedFile != null) {
-      setState(() {
-        _image = File(pickedFile.path);
-      });
+      _image = File(pickedFile.path);
       checkFields();
     }
   }
+}
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: bgColor,
-        body: OrientationBuilder(
-          builder: (context, orientation) {
-            final Size size = MediaQuery.sizeOf(context);
-            final isPortrait = orientation == Orientation.portrait;
-            return SingleChildScrollView(
-              child: Column(
-                children: [
-                  Stack(
-                    children: [
-                      Image.asset(
-                        'assets/images/profilebg.png',
-                        fit: BoxFit.cover,
-                        width: double.infinity,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 16,
-                          vertical: 10,
-                        ),
-                        child: Row(
-                          children: [
-                            IconButton(
-                              onPressed: () {
-                                Navigator.pop(context);
-                              },
-                              icon: Icon(Icons.arrow_back_ios, color: bgColor),
-                            ),
-                            Text(
-                              'Profile',
-                              style: TextStyle(color: bgColor, fontSize: 20),
-                            ),
-                            Spacer(),
-                            IconButton(
-                              onPressed: () {
-                                // Add edit functionality here
-                              },
-                              icon: Icon(Icons.edit, color: bgColor),
-                            ),
-                          ],
-                        ),
-                      ),
-
-                      Center(
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 100),
-                          child: Stack(
+    return ChangeNotifierProvider(
+      create: (context) => ProfileProvider(),
+      child: SafeArea(
+        child: Scaffold(
+          backgroundColor: bgColor,
+          body: Consumer<ProfileProvider>(
+            builder: (context, provider, child) {
+              final Size size = MediaQuery.sizeOf(context);
+              return SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Stack(
+                      children: [
+                        Image.asset('assets/images/profilebg.png', fit: BoxFit.cover, width: double.infinity),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                          child: Row(
                             children: [
-                              CircleAvatar(
-                                radius: 50,
-                                backgroundColor: bgColor,
-                                backgroundImage:
-                                    _image != null ? FileImage(_image!) : null,
-                                child:
-                                    _image == null
-                                        ? Image.asset(
-                                          'assets/icons/person.png',
-                                          color: black,
-                                        )
-                                        : null,
+                              IconButton(
+                                onPressed: () => Navigator.pop(context),
+                                icon: Icon(Icons.arrow_back_ios, color: bgColor),
                               ),
-                              Positioned(
-                                bottom: 0,
-                                right: 0,
-                                child: GestureDetector(
-                                  onTap: _pickImage,
-                                  child: CircleAvatar(
-                                    radius: 18,
-                                    backgroundColor: bgColor,
-                                    child: Icon(
-                                      _image==null? Icons.camera_alt:
-                                      Icons.edit,
-                                      color:_image == null ? nexttext : primaryColor,
-                                      size: 18,
-                                    ),
-                                  ),
-                                ),
+                              Text('Profile', style: TextStyle(color: bgColor, fontSize: 20)),
+                              Spacer(),
+                              IconButton(
+                                onPressed: () {},
+                                icon: Icon(Icons.edit, color: bgColor),
                               ),
                             ],
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 15),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Your Profile',
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontFamily: 'PPNeueMontreal',
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                        Text(
-                          'Provide your basic details for a personalized experience.',
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontFamily: 'HelveticaNeueMedium',
-                            fontWeight: FontWeight.w600,
-                            color: grey,
-                          ),
-                        ),
-                        SizedBox(height: 30),
-                        buildTextField('First Name', _fnameController, 'Your first name'),
-                        SizedBox(height: 10),
-                        buildTextField('Last Name', _lnameController, 'Your last name'),
-                        SizedBox(height: 10),
-                        buildTextField('Company Name', _cnameController, 'Your company name'),
-                        SizedBox(height: size.height * 0.08),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 5),
-                          child: SizedBox(
-                            height: isPortrait ? size.height * 0.07 : size.height * 0.1,
-                            width: isPortrait ? double.infinity : size.width * 01,
-                            child: ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor:
-                                    isButtonEnabled ? primaryColor: nextbg,
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(16),
+                        Center(
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 100),
+                            child: Stack(
+                              children: [
+                                CircleAvatar(
+                                  radius: 50,
+                                  backgroundColor: bgColor,
+                                  backgroundImage: provider.image != null ? FileImage(provider.image!) : null,
+                                  child: provider.image == null ? Image.asset('assets/icons/person.png', color: black) : null,
                                 ),
-                              ),
-                              onPressed: isButtonEnabled
-                                  ? () {
-                                      // Next button pressed logic
-                                      Navigator.push(context, MaterialPageRoute(builder: (context) =>  AiWelcome(name: _fnameController.text,)));
-                                    }
-                                  : null,
-                              child: Text(
-                                'Next',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w500,
-                                  color: isButtonEnabled ? Colors.white : nexttext,
+                                Positioned(
+                                  bottom: 0,
+                                  right: 0,
+                                  child: GestureDetector(
+                                    onTap: provider.pickImage,
+                                    child: CircleAvatar(
+                                      radius: 18,
+                                      backgroundColor: bgColor,
+                                      child: Icon(
+                                        provider.image == null ? Icons.camera_alt : Icons.edit,
+                                        color: provider.image == null ? nexttext : primaryColor,
+                                        size: 18,
+                                      ),
+                                    ),
+                                  ),
                                 ),
-                              ),
+                              ],
                             ),
                           ),
                         ),
                       ],
                     ),
-                  ),
-                ],
-              ),
-            );
-          }
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 15),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Your Profile', style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600)),
+                          Text('Provide your basic details for a personalized experience.', style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600, color: grey)),
+                          SizedBox(height: 30),
+                          buildTextField('First Name', provider.fnameController, 'Your first name'),
+                          SizedBox(height: 10),
+                          buildTextField('Last Name', provider.lnameController, 'Your last name'),
+                          SizedBox(height: 10),
+                          buildTextField('Company Name', provider.cnameController, 'Your company name'),
+                          SizedBox(height: size.height * 0.08),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 5),
+                            child: SizedBox(
+                              height: size.height * 0.07,
+                              width: double.infinity,
+                              child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: provider.isButtonEnabled ? primaryColor : nextbg,
+                                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+                                ),
+                                onPressed: provider.isButtonEnabled
+                                    ? () => Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                            builder: (context) => AiWelcome(name: provider.fnameController.text),
+                                          ),
+                                        )
+                                    : null,
+                                child: Text(
+                                  'Next',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w500,
+                                    color: provider.isButtonEnabled ? Colors.white : nexttext,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
         ),
       ),
     );
@@ -222,15 +177,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(
-          label,
-          style: TextStyle(
-            fontSize: 15,
-            fontFamily: 'HelveticaNeueMedium',
-            fontWeight: FontWeight.w600,
-            color: black,
-          ),
-        ),
+        Text(label, style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600, color: black)),
         SizedBox(height: 10),
         TextFormField(
           controller: controller,
@@ -240,16 +187,9 @@ class _ProfileScreenState extends State<ProfileScreen> {
             filled: true,
             fillColor: phonefieldColor,
             hintText: hint,
-            hintStyle: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w500,
-              color: phonefieldtext,
-            ),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(16),
-              borderSide: BorderSide.none,
-            ),
-            counterText: "", // Hide default counter
+            hintStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: phonefieldtext),
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(16), borderSide: BorderSide.none),
+            counterText: "",
           ),
         ),
       ],
