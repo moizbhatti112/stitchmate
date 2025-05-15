3349631b1b2571f2fd210e49b2202d85a0e94230
moizbhatti112 <moizbhatti112@gmail.com>
2025-05-15 21:32:01 +0500

    Payment method added

--- lib/core/constants/stripe_keys.dart
+++ lib/core/constants/stripe_keys.dart
@@ -3,4 +3,4 @@
 const String stripePublishableKey =
     "pk_test_51RN8U1LzEyfnTKfWXRn8UqTlTbT1aV6tz4P9eRZHzR1B999n4gBRUdRts6gNmFPMeVIr69emHwgsh3MSlXKN1VIo00E5Ejy1FR";
 const String stripeSecretKey =
-    "Your_Scret_Key"; // Replace with your actual secret key
+    "Your_Secret_Key"; // Replace with your actual secret key
